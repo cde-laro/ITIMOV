f53af9572aee67b04c4d01ee284e256058910234
cde-laro <cde-laro@student.42.fr>
2025-07-13 16:03:07 +0200

    fix render

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 # filepath: /Users/cdelarocque/ITINOV/entrypoint.sh
 
 # Lancer le back NestJS en arrière-plan
-cd /app/back && npm run start:prod &
+cd /app/back && npm run start &
 
 # Lancer le front Next.js (en mode production)
 cd /app/front && npm run start
