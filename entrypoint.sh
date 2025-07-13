2b4a231788e67d7a7199306e46b0eb455b01af49
cde-laro <cde-laro@student.42.fr>
2025-07-13 16:13:57 +0200

    remove monolitic docker

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# filepath: /Users/cdelarocque/ITINOV/entrypoint.sh
-
-# Lancer le back NestJS en arrière-plan
-cd /app/back && npm run start &
-
-# Lancer le front Next.js (en mode production)
-cd /app/front && npm run start
